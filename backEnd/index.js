421a2bda64249f1b6da00c4796bae50772c04cee
mikaelangelo12 <mikaelangelo12@gmail.com>
2021-09-17 23:09:50 -0300

    fazendo consign ter acesso a api

--- backEnd/index.js
+++ backEnd/index.js
@@ -3,7 +3,10 @@ const consign = require('consign')
 
 consign()
   .then('./config/middleware.js')
+  .then('./api')
+  .then('./config/routes.js')
   .into(app)
+
 app.listen(3000, ()=>{
   console.log("BackEnd Rodando")
 })
