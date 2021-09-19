db5393241f46b261422ffb7c40679b61fea494eb
mikaelangelo12 <mikaelangelo12@gmail.com>
2021-09-18 23:14:35 -0300

    Criando função que vai verificar se existe o valor

---
+++ backEnd/api/validation.js
@@ -0,0 +1,6 @@
+function existirValor(value, msg){
+  if(!value) throw msg
+  if(Array.isArray(value) && value.length === 0) throw msg
+  if(typeof value === 'string' && !value.trim()) throw msg
+
+}
