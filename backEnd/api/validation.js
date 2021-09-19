39438a1fb005f6c859296e50d88cffe3a7fc3c8b
mikaelangelo12 <mikaelangelo12@gmail.com>
2021-09-18 23:24:50 -0300

    finalizando a validação

--- backEnd/api/validation.js
+++ backEnd/api/validation.js
@@ -1,16 +1,19 @@
-function existirValor(value, msg){
-  if(!value) throw msg
-  if(Array.isArray(value) && value.length === 0) throw msg
-  if(typeof value === 'string' && !value.trim()) throw msg
-}
-
-function naoExistiValor(value, msg){
-  try{
-    existirError(value, msg)
-  } catch(msg){
-    return
+module.exports = app => {
+  function existirValor(value, msg){
+    if(!value) throw msg
+    if(Array.isArray(value) && value.length === 0) throw msg
+    if(typeof value === 'string' && !value.trim()) throw msg
   }
-}
-function existeDoisValores(valueA, valueB, msg){
-  if(valueA !== valueB) throw msg
+
+  function naoExistiValor(value, msg){
+    try{
+      existirError(value, msg)
+    } catch(msg){
+      return
+    }
+  }
+  function existeDoisValores(valueA, valueB, msg){
+    if(valueA !== valueB) throw msg
+  }
+  return {existeDoisValores, naoExistiValor, existirValor}
 }
