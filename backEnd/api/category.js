0953d8e2030bc3602b2c230e352f14fff6580c05
mikaelangelo12 <mikaelangelo12@gmail.com>
2021-09-19 13:37:05 -0300

    Criando api category

--- backEnd/api/category.js
+++ backEnd/api/category.js
@@ -1,3 +1,12 @@
 module.exports = app => {
+  const {  naoExistiValor, existirValor } = app.api.validation
 
+  const save = (req, res) => {
+    const category = {
+      id: req.body.id,
+      name: req.body.name,
+      parentId: req.body.parentId
+  }
+
+  }
 }
